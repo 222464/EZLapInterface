9eea8740a30a9d10b908cd1457bb94920c382016
Eric Laukien <ericlaukien@gmail.com>
2026-04-09 20:28:40 -0400

    Terminal UI

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
 import numpy as np
-import pygame
-import pygame.freetype
 import threading
 from ezlap_reader import EZLapReader
 from tracker import Tracker
@@ -47,6 +45,10 @@ def log_data(data):
 
             latest.insert(0, (data[0], result, time.time()))
 
+            text = f'Lap from {latest[0][0]}, {latest[0][1]/1000.0:.2f}s'
+
+            print(text)
+
             # discard older
             if len(latest) > last_n:
                 latest = latest[:last_n]
@@ -54,53 +56,24 @@ def log_data(data):
 reader_thread = threading.Thread(target=reader_func, daemon=True)
 reader_thread.start()
 
-pygame.init()
-screen = pygame.display.set_mode((800, 480))
-
-font = pygame.freetype.Font('terminal.ttf', 30)
-
 running = True
 
-start_time = pygame.time.get_ticks()
+start_time = time.time()
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    ks = pygame.key.get_pressed()
-
-    if ks[pygame.K_q]:
-        running = False
-        
     if len(speech) > 0:
         engine.say(speech[0])
         engine.runAndWait()
 
         speech = []
 
-    #screen.blit(surf, (0, 0))
-    screen.fill((0, 0, 0))
-
-    # heading
-    text_surface, rect = font.render('UID, LAPTIME', (32, 255, 32))
-
-    screen.blit(text_surface, (10, 10))
-
-    for i, r in enumerate(latest):
-        text_surface, rect = font.render(f'{r[0]}, {r[1]/1000.0:.2f}', (32, 255, 32))
-
-        screen.blit(text_surface, (10, 10 + 32 * (1 + i)))
-
-    pygame.display.flip()
-
-    end_time = pygame.time.get_ticks()
+    end_time = time.time()
 
     dt = end_time - start_time
 
     start_time = end_time
 
-    pygame.time.wait(max(0, 1000 // fps - dt))
+    time.sleep(max(0.0, 1.0 / fps - dt))
 
 reader.close()
 reader_thread.join()
